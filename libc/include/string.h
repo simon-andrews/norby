1304c701aeea520db8f9b2190d7c9d12f3aa581a
Simon Andrews <simon.andrews212@gmail.com>
2016-05-22 11:01:26 -0400

    Add memcpy function

--- libc/include/string.h
+++ libc/include/string.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+void* memcpy(void* dest, const void* src, size_t n);
 void* memset(void* destination, int chr, size_t count);
 size_t strlen(const char* str);
 
--- libc/string.c
+++ libc/string.c
@@ -1,6 +1,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+void* memcpy(void* dest, const void* src, size_t n) {
+  uint8_t* u8_dest = (uint8_t*) dest;
+  const uint8_t* u8_src = (const uint8_t*) src;
+  for(unsigned int i = 0; i < n; i++) {
+    u8_dest[i] = u8_src[i];
+  }
+  return u8_dest;
+}
+
 void* memset(void* destination, int chr, size_t count) {
   uint8_t* buffer = (uint8_t*) destination;
   for(unsigned int i = 0; i < count; i++) {
