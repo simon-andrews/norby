05a2b367b649928bee66e8acbe2d8ef3440d36a9
Simon Andrews <simon.andrews212@gmail.com>
2016-06-27 15:57:22 -0400

    Add everything to header file

--- libc/include/sys/io.h
+++ libc/include/sys/io.h
@@ -3,7 +3,11 @@
 
 #include <stdint.h>
 
-unsigned char inb(uint16_t port);
+uint8_t inb(uint16_t port);
 void outb(uint16_t port, uint8_t value);
+uint16_t inw(uint16_t port);
+void outw(uint16_t port, uint16_t value);
+uint32_t inl(uint16_t port);
+void outl(uint16_t port, uint32_t value);
 
 #endif
