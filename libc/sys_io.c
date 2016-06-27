96cbf06c4991ced8de399b8485bda9c79a7f0864
Simon Andrews <simon.andrews212@gmail.com>
2016-06-27 15:45:02 -0400

    unsigned char -> uint8_t

--- libc/sys_io.c
+++ libc/sys_io.c
@@ -4,8 +4,8 @@ void outb(uint16_t port, uint8_t value) {
   asm volatile ("outb %1, %0" : : "dN" (port), "a" (value));
 }
 
-unsigned char inb(uint16_t port) {
-  unsigned char ret;
+uint8_t inb(uint16_t port) {
+  uint8_t ret;
   asm volatile ("inb %1, %0" : "=a" (ret) : "dN" (port));
   return ret;
 }
