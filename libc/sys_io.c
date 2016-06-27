d8ca4d58697121b9866e94860d1c0c4466eed32c
Simon Andrews <simon.andrews212@gmail.com>
2016-06-27 15:56:03 -0400

    Add outw and inw

--- libc/sys_io.c
+++ libc/sys_io.c
@@ -10,6 +10,16 @@ uint8_t inb(uint16_t port) {
   return ret;
 }
 
+void outw(uint16_t port, uint16_t value) {
+  asm volatile ("outw %1, %0" : : "dN" (port), "a" (value));
+}
+
+uint16_t inw(uint16_t port) {
+  uint16_t ret;
+  asm volatile ("inw %1, %0" : "=a" (ret) : "dN" (port));
+  return ret;
+}
+
 void outl(uint16_t port, uint32_t value) {
   asm volatile ("outl %1, %0" : : "dN" (port), "a" (value));
 }
