342b576de5cc3fd0aad536171d68b414ffc0fc3d
Simon Andrews <simon.andrews212@gmail.com>
2016-06-27 15:44:16 -0400

    Add inl and outl functions

--- libc/sys_io.c
+++ libc/sys_io.c
@@ -9,3 +9,13 @@ unsigned char inb(uint16_t port) {
   asm volatile ("inb %1, %0" : "=a" (ret) : "dN" (port));
   return ret;
 }
+
+void outl(uint16_t port, uint32_t value) {
+  asm volatile ("outl %1, %0" : : "dN" (port), "a" (value));
+}
+
+uint32_t inl(uint16_t port) {
+  uint32_t ret;
+  asm volatile ("inl %1, %0" : "=a" (ret) : "dN" (port));
+  return ret;
+}
