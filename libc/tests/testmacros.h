9c196c679ab572b7d6f9a5c0b9a8d784ab07979d
Simon Andrews <simon.andrews212@gmail.com>
2016-07-01 14:08:02 -0400

    Add include guard to testmacros.h

--- libc/tests/testmacros.h
+++ libc/tests/testmacros.h
@@ -1,5 +1,10 @@
+#ifndef NORBY_LIBC_TEST_MACROS_H
+#define NORBY_LIBC_TEST_MACROS_H
+
 //It would probably be better to call these assert_not_uneq and assert_not_eq
 #define ASSERT_EQ(a, b) {if(a != b) return 1; }
 #define ASSERT_NQ(a, b) {if(a == b) return 1; }
 
 #define ASSERT(expr) { if(!(expr)) return 1; }
+
+#endif
